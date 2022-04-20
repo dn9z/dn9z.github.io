69df49c8a9579f1e2276b71362c8ccb3e7a01fac
dn9z <28568855+dn9z@users.noreply.github.com>
2022-04-21 00:37:52 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.scss";
 import Navbar from "./components/Navbar/Navbar";
-import { Routes, Route, useLocation } from "react-router-dom";
+import { Routes, Route, useLocation, Navigate } from "react-router-dom";
 import Home from "./routes/Home/Home";
 import About from "./routes/About/About";
 import Work from "./routes/Work/Work";
@@ -9,6 +9,7 @@ import { AnimatePresence } from "framer-motion";
 import EnvAnims from "./components/anim/EnvAnims/EnvAnims";
 import { MyProvider } from "./components/Context/Context";
 import Background from "./components/Background/Background";
+import PageNotFound from "./routes/PageNotFound/PageNotFound";
 
 function App() {
   const location = useLocation();
@@ -17,39 +18,40 @@ function App() {
     <MyProvider>
       {/* <AnimatePresence exitBeforeEnter> */}
       <main
-          // Hide scrollbar for 8 seconds
-          // key={location.pathname}
-          // animate={{ overflowY: "visible", transition: { delay: 8 } }}
-          // exit={{ overflowY: "hidden" }}
-        >
-          <Background />
-          <Navbar />
-          <section>
-            <AnimatePresence exitBeforeEnter>
-              <EnvAnims key={location.pathname} />
-            </AnimatePresence>
+      // Hide scrollbar for 8 seconds
+      // key={location.pathname}
+      // animate={{ overflowY: "visible", transition: { delay: 8 } }}
+      // exit={{ overflowY: "hidden" }}
+      >
+        <Background />
+        <Navbar />
+        <section>
+          <AnimatePresence exitBeforeEnter>
+            <EnvAnims key={location.pathname} />
+          </AnimatePresence>
 
-            <motion.div
-              className="content-container"
-              animate={
-                location.pathname !== "/"
-                  ? { y: "-10rem", transition: { delay: 2.3 } }
-                  : ""
-              }
-              exit={{ transition: { duration: 1 } }}
-            >
-              <AnimatePresence exitBeforeEnter>
-                <Routes location={location} key={location.pathname}>
-                  <Route path="/" element={<Home />} />
-                  <Route path="/about" element={<About />} />
-                  <Route path="/work" element={<Work />} />
-                </Routes>
-              </AnimatePresence>
-            </motion.div>
-          </section>
-        </main>
+          <motion.div
+            className="content-container"
+            animate={
+              location.pathname !== "/"
+                ? { y: "-10rem", transition: { delay: 2.3 } }
+                : ""
+            }
+            exit={{ transition: { duration: 1 } }}
+          >
+            <AnimatePresence exitBeforeEnter>
+              <Routes location={location} key={location.pathname}>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/work" element={<Work />} />
+                <Route path="/404" element={<PageNotFound />} />
+                <Route path="*" element={<Navigate replace to="/404" />} />
+              </Routes>
+            </AnimatePresence>
+          </motion.div>
+        </section>
+      </main>
       {/* </AnimatePresence> */}
-       
     </MyProvider>
   );
 }
